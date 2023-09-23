ced6c4f45eb950356bc3b4f4f71a3257b3355e26
Erkam Esen <120065120+erkamesen@users.noreply.github.com>
2023-09-23 10:28:12 +0300

    Add files via upload

---
+++ aws.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get update 
+sudo apt-get -y upgrade
+
+sudo apt-get install python3-venv
+python3 -m /root/ubuntu/home/venv venv
+
+cd /root/ubuntu/home
+
+read -p "GitHub Repository URL: " github_url
+
+read -p "Project Name :  project_name
+git clone "$github_url"
+cd "$project_name"
+pip install -r requirements.txt
+pip install gunicorn
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+sudo apt-get install nginx
+sudo apt-get install supervisor
+sudo mkdir /var/log/gunicorn/
+
+sudo touch /etc/nginx/sites-available/django.conf
+sudo ln /etc/nginx/sites-available/django.conf /etc/nginx/sites-enable/django.conf
+
+sudo touch /etc/supervisor/conf.d/gunicorn.conf
